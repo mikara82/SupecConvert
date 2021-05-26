a7d24c1d718e6c3d827b097e66959a7b1ab9c2a9
mikara82 <michael-raab@gmx.de>
2021-05-26 07:13:59 +0200

    initial

---
+++ Source/SupecConvert/SupecConvertGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SupecConvertGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPECCONVERT_API ASupecConvertGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
